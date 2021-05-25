e766820393044d848100ab4b8b51ff9396c0d75b
extremelf <luis.paulo.6.1.2001@gmail.com>
2021-05-25 23:25:15 +0100

    second commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { NavController, IonSlide } from '@ionic/angular';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
+
 export class Tab1Page {
-
-  constructor() {}
-
+  slidesOptions = {
+    initialSlide: 0,
+    direction: 'horizontal',
+    speed: 300,
+    spaceBetween: 8,
+    slidesPerView: 3,
+    freeMode: true,
+    loop: false
+  };
+  constructor(public navCtrl: NavController) {}
+  
 }
