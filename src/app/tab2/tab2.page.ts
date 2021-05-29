46db947afb6b2ab0add83916b0968ee26bf50e27
extremelf <luis.paulo.6.1.2001@gmail.com>
2021-05-29 14:45:59 +0100

    intermedio

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,27 +1,58 @@
-import { Component, QueryList, ViewChildren, ElementRef } from '@angular/core';
-import { ToastController, IonList, IonItem } from '@ionic/angular';  
+import { Component, QueryList, ViewChildren, ElementRef, OnInit } from '@angular/core';
+import { IonCard, IonImg } from '@ionic/angular';  
+import { MovieService } from '../services/movie.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit{
 
   public tipoClass: any;
-  
-  constructor(public toastCtrl: ToastController) {}
+  public actionMovies: any;
+  public adventureMovies: any;
+  public thrillerMovies: any;
 
-  @ViewChildren(IonItem,{read: ElementRef}) lista: QueryList<ElementRef>;
+
+  slidesOptions = {
+    initialSlide: 0,
+    direction: 'vertical',
+    speed: 300,
+    spaceBetween: 8,
+    slidesPerView: 3,
+    freeMode: true,
+    loop: false
+  };
+  
+  constructor(private movieServ: MovieService ) {}
+
+  
+
+  ngOnInit(){
+    this.movieServ.getActionMovies().subscribe( movies => {
+      this.actionMovies = movies;
+    });
+
+    this.movieServ.getAdventureMovies().subscribe( movies => {
+      this.adventureMovies = movies;
+    })
+
+    this.movieServ.getThrillerMovies().subscribe( movies => {
+      this.thrillerMovies = movies;
+    })
+  }
+
+  @ViewChildren(IonCard,{read: ElementRef}) lista: QueryList<ElementRef>;
 
   filterSearch(evt){
-    this.lista.forEach(item =>{
-      console.log(item.nativeElement.inneHTML);
-      
-      const show = item.nativeElement.innerHTML.toLowerCase().indexOf(evt.target.value.toLowerCase()) > -1;
-      //console.log(item);
+    this.lista.forEach(item =>{    
+      const show = item.nativeElement.children[1].innerHTML.toLowerCase().indexOf(evt.target.value.toLowerCase()) > -1;
       item.nativeElement.style.display = show? 'block' : 'none';
-      //item.nativeElement.attributes.class.nodeValue = show ? 'ion-hidden' : 'item md in-list ion-focusable hydrated';
     });
+  }
+
+  verMovie(rota: string, numero: string, movie: any) {
+    this.movieServ.goRota(rota,numero, movie);
   }
 }
