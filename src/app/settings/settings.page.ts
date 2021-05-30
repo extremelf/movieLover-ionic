985b2351ccc1a6bd8c504910b45689978b8cbef0
Joel <joel.eu.lopes@gmail.com>
2021-05-30 03:07:32 +0100

    perfil 80%

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import FormJSon from '../../assets/loginData.json';
 import RootObject = namespace.RootObject;
+import {Router} from "@angular/router";
+import {NavController} from "@ionic/angular";
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
 declare namespace namespace {
@@ -18,15 +20,32 @@ declare namespace namespace {
   styleUrls: ['./settings.page.scss'],
 })
 
-export class SettingsPage implements OnInit {
+export class SettingsPage {
   myform: FormGroup;
   dataForm = FormJSon;
+  buttonform: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  /**
+  public var username:string ="GoGoGO";
+  public var pass:string = "1234";
+**/
+
+
+  constructor(private fb: FormBuilder, private route: Router,public navController: NavController, public formBuilder: FormBuilder ) {
     this.myform= this.fb.group({});
     this.creteControls(this.dataForm);
+    /**
+     *Outra tentativa de building
+     */
+    this.buttonform = formBuilder.group({
+      pass1:['',Validators.compose([Validators.required])],
+      pass2:['',Validators.compose([Validators.required])]
+    });
   }
 
+  nextPage(){
+    this.route.navigate(['/tab3']);
+  }
 
   creteControls(controls: Array<RootObject>){
   for (const control of controls){
@@ -40,7 +59,6 @@ export class SettingsPage implements OnInit {
   console.log('My form ', this.myform);
 
 }
-  ngOnInit() {
-  }
+
 
 }
